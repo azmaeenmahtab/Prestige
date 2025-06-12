eeb8daa2479642851ae4d17e881fc1811541a290
Azmaeen Mahtab <mahtabazmaeen7@gmail.com>
2025-06-12 16:11:24 +0600

    feature 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Dashboard from './components/dashboard'
+import Framer from './components/framer'
+import Feature1 from './components/feature1'
 
 function App() {
    
@@ -14,6 +16,8 @@ function App() {
        <Navbar />
        <Hero />
        <Dashboard  />
+       <Framer />
+       <Feature1 />
     </>
   )
 }
